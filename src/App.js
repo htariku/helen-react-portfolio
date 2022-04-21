1e073f28332de26dd127e6abcca10faf458ccb04
htariku <hgtariku@yahoo.com>
2022-04-20 20:40:51 -0400

    updated app.js with fontawsome

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import resumeData from './resumeData';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header resumeData={resumeData}/>
-        <About resumeData={resumeData}/>
-        <Resume resumeData={resumeData}/>
-        <Portfolio resumeData={resumeData}/>
-        <Contact resumeData={resumeData}/>
-        <Footer resumeData={resumeData}/>
-      </div>
-    );
-  }
+import React, { useState } from "react";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Nav from "./components/Nav";
+import ContactForm from "./components/Contact";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+library.add(fab)
+
+
+function App() {
+  const [categories] = useState([
+    {
+      name: "projects",
+      description:
+        "Picture of projects",
+    },
+    
+
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
+  return (
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Projects currentCategory={currentCategory}></Projects>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
+    
+
+        <h2> Made by Helen Tariku <a href="https://github.com/htariku"><FontAwesomeIcon icon={["fab","github"]}></FontAwesomeIcon></a>&nbsp;&nbsp;<a href="https://www.linkedin.com/in/helen-tariku-5283aa156/"><FontAwesomeIcon icon={["fab","linkedin"]}></FontAwesomeIcon></a>  </h2>
+         
+    </div>
+    
+  );
 }
 
 export default App;
